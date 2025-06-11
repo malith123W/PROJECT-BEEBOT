9dc59c66fd2b69545aae4b88ada0136a73e7bb73
malith123W <malithinduranga2001@gmail.com>
2025-06-11 20:51:00 +0530

    edit

--- platform_pc/Generate AI_Maker/generate_maker.py
+++ platform_pc/Generate AI_Maker/generate_maker.py
@@ -7,5 +7,7 @@ dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_250)
 # Generate the marker
 marker_size = 200  # Size in pixels
 marker_id = 3     # ID of the marker to generate
+
+
 markerImage = cv.aruco.generateImageMarker(dictionary, marker_id, marker_size)
 cv.imwrite("marker3.png", markerImage)
